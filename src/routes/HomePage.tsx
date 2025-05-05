b029bc0327fc9ebc2720072c1a9a8174f8ecffa4
kardelenoglak <koglakci@ford.com.tr>
2025-05-06 00:58:04 +0300

    HomePage created

---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect } from 'react';
+import {
+    TextField,
+    Select,
+    MenuItem,
+    Grid,
+    CircularProgress,
+    Typography,
+    InputLabel,
+    FormControl,
+    Pagination,
+} from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMovies, setSearch, setType, setYear, setPage } from '../redux/movieSlice';
+import { RootState } from '../redux/store';
+import { AppDispatch } from '../redux/store';
+
+const Home: React.FC = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const {
+        list,
+        loading,
+        search,
+        year,
+        type,
+        page,
+        totalResults,
+    } = useSelector((state: RootState) => state.movies);
+
+    useEffect(() => {
+        dispatch(fetchMovies());
+    }, [search, year, type, page]);
+
+    const handleRowClick = (params: any) => {
+        navigate(`/movie/${params.row.imdbID}`);
+    };
+
+    const columns: GridColDef[] = [
+        { field: 'Title', headerName: 'Title', flex: 1 },
+        { field: 'Year', headerName: 'Year', width: 100 },
+        { field: 'imdbID', headerName: 'IMDB ID', width: 200 },
+    ];
+
+    return (
+        <div style={{ padding: '2rem' }}>
+            <Grid container spacing={2} alignItems="center">
+                <Grid size={4}>
+                    <TextField
+                        fullWidth
+                        label="Search"
+                        value={search}
+                        onChange={(e) => dispatch(setSearch(e.target.value))}
+                    />
+                </Grid>
+
+                <Grid size={4}>
+                    <TextField
+                        fullWidth
+                        label="Year"
+                        value={year}
+                        onChange={(e) => dispatch(setYear(e.target.value))}
+                    />
+                </Grid>
+
+                <Grid size={4}>
+                    <FormControl fullWidth>
+                        <InputLabel>Type</InputLabel>
+                        <Select
+                            value={type}
+                            onChange={(e) => dispatch(setType(e.target.value))}
+                            label="Type"
+                        >
+                            <MenuItem value="">All</MenuItem>
+                            <MenuItem value="movie">Movie</MenuItem>
+                            <MenuItem value="series">TV Series</MenuItem>
+                            <MenuItem value="episode">Episode</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+            </Grid>
+
+            <div style={{ marginTop: '2rem' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    {loading ? (
+                        <CircularProgress />
+                    ) : list.length === 0 ? (
+                        <Typography>No results found.</Typography>
+                    ) : (
+                        <>
+                            <div style={{ height: 400, width: '100%' }}>
+                                <DataGrid
+                                    rows={list}
+                                    columns={columns}
+                                    paginationModel={{ pageSize: 10, page: page - 1 }}
+                                    onPaginationModelChange={(model: { page: number }) => dispatch(setPage(model.page + 1))}
+                                    onRowClick={handleRowClick}
+                                    getRowId={(row: any) => row.imdbID}
+                                />
+                            </div>
+
+                            <div style={{ marginTop: '2rem', display: 'flex', justifyContent: 'center' }}>
+                                <Pagination
+                                    count={Math.ceil(totalResults / 10)}
+                                    page={page}
+                                    onChange={(_, value) => dispatch(setPage(value))}
+                                    color="primary"
+                                />
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
