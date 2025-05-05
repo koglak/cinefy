65d22fcf226d2ecb001c123a49913b87b4286a42
kardelenoglak <koglakci@ford.com.tr>
2025-05-06 00:05:56 +0300

    Navbar & style added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Typography, AppBar, Toolbar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import '../styles/navbar.scss';
+
+export default function Navbar() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
+    return (
+        <AppBar position="static" className="navbar">
+            <Toolbar>
+                <div className="navbar-icon-container">
+                    <span className="navbar-icon">🎬</span>
+                </div>
+                <Typography
+                    variant="h4"
+                    className="navbar-logo"
+                    onClick={handleLogoClick}
+                >
+                    Cinefy
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
