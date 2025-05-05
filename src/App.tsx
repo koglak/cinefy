428d244d067b6e8b3e0dc6cb3bd1cd5f31fea80c
kardelenoglak <koglakci@ford.com.tr>
2025-05-06 00:58:26 +0300

    App.tsx & routes implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Home from './routes/HomePage';
+import Detail from './routes/DetailPage';
+import Layout from './layouts/Layout';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<Detail />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
