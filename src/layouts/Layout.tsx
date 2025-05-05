19addcaa40c79f327d9096716d1ca47edf03ec5a
kardelenoglak <koglakci@ford.com.tr>
2025-05-06 00:56:30 +0300

    Layout responsive for different heights

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,14 +1,12 @@
-import { LayoutProps } from '../constants/layout';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { LayoutProps } from '../constants/layout';
 
 export default function Layout({ children }: LayoutProps) {
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Navbar />
-            <main>
-                {children}
-            </main>
+            <main style={{ flex: 1, padding: '2rem' }}>{children}</main>
             <Footer />
         </div>
     );
